a0ff9fcd202412b4c89f76a8509699eeeca1b0ef
OmidMozafari <omidmozafari444@gmail.com>
2026-04-29 12:47:44 +0430

    added some functionalities to the project.

--- src/App.js
+++ src/App.js
@@ -1,83 +1,62 @@
+import { useState } from "react";
+
 export default function App() {
+
+  const productItems = [
+    {name : "Iphone 16", price: "$1200"},
+    {name : "Iphone 16 pro", price: "$1400"},
+    {name : "Iphone 16 pro max", price: "$1800"},
+    {name : "S23 ultra", price: "$1300"},
+    {name : "S22 ultra", price: "$900"},
+    {name : "Poco F3 Xiaomi", price: "$350"},
+    {name : "Poco X3 Xiaomi", price: "$170"},
+    {name : "Poco X5 Xiaomi", price: "$220"},
+    {name : "Honor 8 lite", price: "$120"},
+    {name : "Redmi Note 10+", price: "$180"},
+    {name : "Redmi Note 11", price: "$240"},
+    {name : "Galaxy S3+", price: "$70"},
+  ]
+
+  const [cartItem, setCartItem] = useState([])
+
+  function handleCartItem(e){
+    setCartItem((prev) => [...prev, e.target])
+    console.log(cartItem);
+    
+    
+    
+  }
+  
+
   return (
     <div className="container">
-      <Products />
+      <Products productItems = {productItems} handleCartItem = {handleCartItem} />
       <Cart />
     </div>
   );
 }
 
-function Products (){
+function Products ({productItems, handleCartItem}){
   return <div className="productContainer">
 
     <div className="products-title">
       <h2>Products</h2>
     </div>
-  
-  <div className="productListContainer">
-    <div className="productList">
-      <h3>Mobile</h3>
-      <p>$50</p>
-      <button>Add</button>
-    </div>
 
-    <div className="productList">
-      <h3>Mobile</h3>
-      <p>$50</p>
-      <button>Add</button>
-    </div>
+    <div className="productListContainer">
 
-    <div className="productList">
-      <h3>Mobile</h3>
-      <p>$50</p>
-      <button>Add</button>
-    </div>
+    {productItems.map((v)=>{
 
-    <div className="productList">
-      <h3>Mobile</h3>
-      <p>$50</p>
-      <button>Add</button>
+    return <div className="productList">
+      <h3>{v.name}</h3>
+      <p>{v.price}</p>
+      <button onClick={handleCartItem}>Add</button>
     </div>
+    })}
 
-    <div className="productList">
-      <h3>Mobile</h3>
-      <p>$50</p>
-      <button>Add</button>
     </div>
-
-    <div className="productList">
-      <h3>Mobile</h3>
-      <p>$50</p>
-      <button>Add</button>
-    </div>
-
-    <div className="productList">
-      <h3>Mobile</h3>
-      <p>$50</p>
-      <button>Add</button>
-    </div>
-
-    <div className="productList">
-      <h3>Mobile</h3>
-      <p>$50</p>
-      <button>Add</button>
-    </div>
-
-    <div className="productList">
-      <h3>Mobile</h3>
-      <p>$50</p>
-      <button>Add</button>
-    </div>
-
-    <div className="productList">
-      <h3>Mobile</h3>
-      <p>$50</p>
-      <button>Add</button>
-    </div>
-
   </div>
-  </div>
-}
+  }
 
 function Cart(){
 return <div className="cartContainer">
@@ -86,28 +65,16 @@ return <div className="cartContainer">
     <h1>Cart</h1>
   </div>
 
-  <div className="cartList">
-
+  {/* <div className="cartList">
     <div className="cardListItems">
       <h4>Shoes</h4>
       <p>$100</p>
       <button>❌</button>
     </div>
 
-    <div className="cardListItems">
-      <h4>Shoes</h4>
-      <p>$100</p>
-      <button>❌</button>
-    </div>
 
-    <div className="cardListItems">
-      <h4>Shoes</h4>
-      <p>$100</p>
-      <button>❌</button>
-    </div>
-
-  </div>
-  <Total />
+  </div> */}
+  {/* <Total /> */}
 </div>
 }
 
